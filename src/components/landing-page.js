af94a666ba53621fbe29663d1abe4c6690ad796d
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-15 11:15:15 -0700

    edit to header and landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -12,9 +12,9 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Welcome to Foo App</h2>
+            <h2>Welcome to Speakeasy...do you know the secret knock?</h2>
             <LoginForm />
-            <Link to="/register">Register</Link>
+            <Link to="/register">Become one of the club</Link>
         </div>
     );
 }
