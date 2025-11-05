71cdf24a06c44472af0e314642c836b930ca68b2
maxjeffwell <maxjeffwell@gmail.com>
2025-11-05 14:59:18 -0500

    remove extra text

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Navigate} from 'react-router-dom';
-
 import RegistrationForm from './registration-form';
 
 export function RegistrationPage(props) {
-    // If we are logged in (which happens automatically when registration
-    // is successful) redirect to the learning page
     if (props.loggedIn) {
         return <Navigate to="/learn" replace />;
     }
     return (
         <div className="home">
-            <h2>Register for Foo App</h2>
             <RegistrationForm />
             <Link to="/">Login</Link>
         </div>
